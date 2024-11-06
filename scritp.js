a3356636c42c5502f35d1c54ebeba26131576782
dream1-dream2 <getanand4u1@gmail.com>
2024-11-06 14:57:34 +0530

    first commit

---
+++ scritp.js
@@ -0,0 +1,9 @@
+// let isHappy = "are you happy";
+// confirm(isHappy);
+// alert(isHappy);
+function changeMq() {
+  document.getElementById("t1").innerHTML = "HELLO";
+}
+function changeMq2() {
+  document.getElementById("t1").innerHTML = "aaaaaaaaaaaaaaaaaaaa";
+}
